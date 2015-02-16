b5e7718cfc992c11271eedb21177e406be163ac8
James Heywood <jdheywood@yahoo.co.uk>
2015-02-16 22:28:17 +0000

    Added Api and Spa Azure website projects

---
+++ Solution/Dashboard/Dashboard.Spa/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Dashboard.Spa.Startup))]
+
+namespace Dashboard.Spa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
