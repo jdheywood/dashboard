05de76b3f4623f75f4875f1005a5c6bd30000753
James Heywood <jdheywood@yahoo.co.uk>
2015-02-25 06:54:50 +0000

    Example charts added to dash front end poc

--- dash-poc/dash/js/dash.js
+++ dash-poc/dash/js/dash.js
@@ -35,8 +35,62 @@ function loadCharts() {
 
 				];
 
+	var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+
+	var barChartData = {
+		labels : ["January","February","March","April","May","June","July"],
+		datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,0.8)",
+				highlightFill: "rgba(220,220,220,0.75)",
+				highlightStroke: "rgba(220,220,220,1)",
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+			},
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,0.8)",
+				highlightFill : "rgba(151,187,205,0.75)",
+				highlightStroke : "rgba(151,187,205,1)",
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+			}
+		]
+	};
+	
+	var lineChartData = {
+		labels : ["January","February","March","April","May","June","July"],
+		datasets : [
+			{
+				label: "My First dataset",
+				fillColor : "rgba(220,220,220,0.2)",
+				strokeColor : "rgba(220,220,220,1)",
+				pointColor : "rgba(220,220,220,1)",
+				pointStrokeColor : "#fff",
+				pointHighlightFill : "#fff",
+				pointHighlightStroke : "rgba(220,220,220,1)",
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+			},
+			{
+				label: "My Second dataset",
+				fillColor : "rgba(151,187,205,0.2)",
+				strokeColor : "rgba(151,187,205,1)",
+				pointColor : "rgba(151,187,205,1)",
+				pointStrokeColor : "#fff",
+				pointHighlightFill : "#fff",
+				pointHighlightStroke : "rgba(151,187,205,1)",
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+			}
+		]
+	};
+
 	window.onload = function(){
-		var ctx = document.getElementById("chart-area").getContext("2d");
-		window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});
+		var doughnutContext = document.getElementById("doughnut-area").getContext("2d");
+		window.myDoughnut = new Chart(doughnutContext).Doughnut(doughnutData, {responsive : true});
+
+		var barContext = document.getElementById("bar-area").getContext("2d");
+		window.myBar = new Chart(barContext).Bar(barChartData, {responsive : true});
+
+		var lineContext = document.getElementById("line-area").getContext("2d");
+		window.myLine = new Chart(lineContext).Line(lineChartData, {responsive: true});
 	};
 }
